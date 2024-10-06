d6db7a08c0aff0563b9626cbeeea632c80b50902
David k <sba24003@student.cct.ie>
2024-10-06 22:14:01 +0100

    added main class which adds data to an array.

--- FileReader.java
+++ FileReader.java
@@ -1,6 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class FileReader {
   public static void main(String[] args) {
+    ArrayList<Object> customerDataArray = new ArrayList<Object>();
 
+    try {
+      File customerDataArrayFile = new File("customers.txt");
+
+      Scanner customerScanner = new Scanner(customerDataArrayFile);
+
+      while (customerScanner.hasNextLine()) {
+        String arrayValue = customerScanner.next();
+        customerDataArray.add(arrayValue);
+        
+        System.out.println(customerDataArray);
+         
+      }
+      customerScanner.close();
+    } 
+    
+    catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
     }
-}
+  }
+    }
