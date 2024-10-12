72e4bc23a893a0bb4da24ea85ddd18a8fc63a500
David k <sba24003@student.cct.ie>
2024-10-12 12:43:43 +0100

    added  businessLogic class to calculate finalValue for customer

---
+++ BusinessLogic.java
@@ -0,0 +1,53 @@
+public class BusinessLogic {
+    // calculates the final price price for the customer with their discount
+
+    public static double calculateFinalValue(
+            int customerClass,
+            int lastPurchase,
+            double totalPurchase) {
+
+        double finalValue = 0;
+        int thisYear = 2024;
+
+        if (customerClass == 1) {
+
+            if (lastPurchase == thisYear) {
+                finalValue = totalPurchase * 0.7;
+            }
+
+            else if (lastPurchase < thisYear && lastPurchase > (thisYear - 5)) {
+                finalValue = totalPurchase * 0.8;
+            }
+
+            else {
+                finalValue = totalPurchase * 0.9;
+            }
+        }
+
+        else if (customerClass == 2) {
+
+            if (lastPurchase == thisYear) {
+                finalValue = totalPurchase * 0.85;
+            }
+
+            else if (lastPurchase < thisYear && lastPurchase > (thisYear - 5)) {
+                finalValue = totalPurchase * 0.87;
+            }
+
+            else {
+                finalValue = totalPurchase * 0.95;
+            }
+        }
+
+        else {
+
+            if (lastPurchase == thisYear) {
+                finalValue = totalPurchase * 0.97;
+            }
+
+        }
+
+        return finalValue;
+
+    };
+}
